49b6e3f034374479f2fce0b644e9d31ad1fff5e1
Vale Sarro <valeria.sarro@rootstrap.com>
2022-11-10 19:04:27 -0300

    Feature: Geolocation

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
-import styles from "./index.module.scss";
 import "leaflet/dist/leaflet.css";
 
+import styles from "./index.module.scss";
+
 const Map = () => {
+  const [coords, setCoords] = useState(null);
+  const [latitudeState, longitudeState] = coords ?? [];
+
+  const setCurrentCoords = (location) => {
+    if (location)
+      setCoords([location.coords.latitude, location.coords.longitude]);
+  };
+
+  const handleLocationBlocked = (error) => {
+    if (error.code === error.PERMISSION_DENIED) {
+      setCoords([-33.6003, -69.426086]);
+    }
+  };
+
+  useEffect(() => {
+    if (navigator?.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        setCurrentCoords,
+        handleLocationBlocked
+      );
+    }
+  }, []);
+
+  console.log(latitudeState, longitudeState);
   return (
     <div className={styles.map}>
-      <MapContainer className={styles.map__container} center={[0, 0]} zoom={15}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      </MapContainer>
+      {coords ? (
+        <MapContainer
+          className={styles.map__container}
+          center={[latitudeState, longitudeState]}
+          zoom={15}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        </MapContainer>
+      ) : (
+        <p> Loading . . . </p> // TODO: Make it facherito
+      )}
     </div>
   );
 };
