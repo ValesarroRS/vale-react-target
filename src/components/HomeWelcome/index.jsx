46e20abb0858d2c44b4e0ea2d7f9bd951813ae75
Vale Sarro <valeria.sarro@rootstrap.com>
2022-11-23 10:06:07 -0300

    Updates on home welcome page

--- src/components/HomeWelcome/index.jsx
+++ src/components/HomeWelcome/index.jsx
@@ -4,21 +4,31 @@ import Title from "components/shared/Title";
 import PrimaryParagraph from "components/shared/Paragraph";
 import Button from "components/shared/Button";
 import PointedListElement from "components/shared/PointedListElement";
+import { useNavigate } from "react-router-dom";
 
-const HomeWelcome = () => (
-  <>
-    <img src={Smilies} alt="Smilies" />
-    <div className={styles.welcomeTitle}>
-      <Title variant="lowerCase" text="Welcome to " />
-      <Title text="Target" />
-    </div>
-    <PrimaryParagraph text="Find people near you & Connect" />
-    <div className={styles.pointedList}>
-      <PointedListElement text="Create a target by clicking wherever on the map, specify the ratio and and a topic: Travel, Dating, Music, etc." />
-      <PointedListElement text="T A R G E T will start a chat whenever you’ve a match. You can always dismiss a conversation if you’re not interested." />
-    </div>
-    <Button text="ok; got it!" />
-  </>
-);
-
+const HomeWelcome = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      <img src={Smilies} alt="Smilies" />
+      <div className={styles.welcomeTitle}>
+        <Title variant="lowerCase" text="Welcome to " />
+        <Title text="Target" />
+      </div>
+      <PrimaryParagraph text="Find people near you & Connect" />
+      <div className={styles.pointedList}>
+        <PointedListElement>
+          Create a target by clicking wherever on the map, specify the ratio and
+          and a topic: Travel, Dating, Music, etc.
+        </PointedListElement>
+        <PointedListElement>
+          <b className={styles.highlightWord}>TARGET</b> will start a chat
+          whenever you’ve a match. You can always dismiss a conversation if
+          you’re not interested.
+        </PointedListElement>
+      </div>
+      <Button text="ok; got it!" onClick={() => navigate("/homeEmptyState")} />
+    </>
+  );
+};
 export default HomeWelcome;
