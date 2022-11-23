79074d60ccf61936f459955b51359157022a9770
Vale Sarro <valeria.sarro@rootstrap.com>
2022-11-23 10:05:20 -0300

    Add home menu element

--- src/components/HomeMenu/index.jsx
+++ src/components/HomeMenu/index.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-const HomeMenu = () => {
-  return (
-    <div className="HomeMenu">
-      <button>X</button>
-      <div id="Options">
-        <p>About</p>
-        <p>Contact</p>
-      </div>
+const HomeMenu = () => (
+  <div className="HomeMenu">
+    <button>X</button>
+    <div id="Options">
+      <p>About</p>
+      <p>Contact</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default HomeMenu;
